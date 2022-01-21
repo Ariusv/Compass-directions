71092e21f9fbe32dd4e0d42ee1544c24da3a402c
Vitalii Nastosiak <75575669+Ariusv@users.noreply.github.com>
2022-01-21 21:45:46 +0200

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,9 +2,16 @@ def direction(facing, turn):
     degree = 45
     compass_points = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
 
-    start_index = compass_points.index(facing)
+    try:
+        start_index = compass_points.index(facing)
+    except ValueError:
+        return 'Not correct direction'
 
-    turns = turn // degree
+    try:
+        turns = turn // degree
+    except TypeError:
+        return 'Not correct turn'
+
     target_index = (start_index + turns) % len(compass_points)
 
     return compass_points[target_index]
